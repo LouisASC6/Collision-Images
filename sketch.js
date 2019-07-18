d1401578cfc0fec89dfd108ebfd10041df311a9e
LouisASC6 <louismm16@gmail.com>
2019-07-18 10:55:41 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -2,15 +2,18 @@
 let myposX = 25;
 let myposY = 25;
 let myspeed = 5;
-let enemyX;
-let enemyY;
 let enemyPosX, enemyPosY;
 let enemywidth = 50, enemyheight = 50;
+let mywidth = 50;
+let myheight = 50; 
 
 let myLeft, myRight, myTop, myBottom;
 let enemyLeft, enemyRight, enemyTop, enemyBottom; 
 
 let points = 0
+
+let clefairy;
+let togepi;
 
 function setup() {
     createCanvas(500, 500);
@@ -24,12 +27,19 @@ function setup() {
     enemyPosX = random(25, 475);
     enemyPosY = random(100, 475);
 
+    clefairy = loadImage("clefairy.png");
+    togepi = loadImage("togepi.png");
+
 }
     
 function draw() {
     // refresh the background
     background(0);
     
+    image(togepi, enemyPosX, enemyPosY, enemywidth, enemyheight);
+
+    image(clefairy, myposX, myposY, mywidth, myheight);
+
     // draw the enemy 
     fill(255, 0, 0);
     rect(enemyPosX, enemyPosY, enemywidth, enemyheight);
@@ -102,10 +112,143 @@ function draw() {
         points++;
 
         // generate new enemy position
-        enemyX = random(25, 475);
-        enemyY = random(150, 450);
+        enemyPosX = random(25, 475);
+        enemyPosY = random(150, 450);
     }
 
     fill(255);
     text("Enemies collected: ", + points, 340, 25);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // declare variables
+// let myX = 25;
+// let myY = 25;
+// let myspeed = 5;
+// let myHeight = 50;
+// let myWidth = 50;
+// let myLeft, myRight, myTop, myBottom;
+
+// let enemyLeft, enemyRight, enemyTop, enemyBottom; 
+// let enemyX, enemyY;
+// let enemywidth = 50, enemyheight = 50;
+
+// let points = 0
+
+// let clefairy;
+// let togepi;
+
+// function setup() {
+//     createCanvas(500, 500);
+//     background(0);
+//     textSize(15);
+
+//     //switch rectangle
+//     rectMode(CENTER);
+
+//     // determine the random starting postiton of enemy
+//     enemyPosX = random(25, 475);
+//     enemyPosY = random(100, 475);
+
+//     clefairy = loadImage("clefairy.png");
+//     togepi = loadingimage("togepi.png");
+// }
+    
+// function draw() {
+//     // refresh the background
+//     background(0);
+
+//     image(togepi, enemyX, enemyY, enemywidth, enemyheight);
+
+//     image(clefairy, myX, myY, myWidth, myHeight);
+    
+//     // draw the enemy 
+//     fill(255, 0, 0);
+//     rect(enemyX, enemyY, enemywidth, enemyheight);
+    
+//     // draw myself
+//     fill(0, 0, 255);
+//     rect(myX, myY, 50, 50);
+
+//     // if the LEFT_ARROW is pressed 
+//     if(keyIsDown(LEFT_ARROW)) {
+//         myX -= myspeed;
+//     }
+
+//     // if the RIGHT_ARROW is pressed
+//     if(keyIsDown(RIGHT_ARROW)) {
+//         myX += myspeed;
+//     }
+
+//     // if UP_ARROW is pressed
+//     if(keyIsDown(UP_ARROW)) {
+//         myposY -= myspeed;
+//     }
+
+//     // if DOWN_ARROW is pressed
+//     if(keyIsDown(DOWN_ARROW)) {
+//         myY += myspeed;
+//     }
+
+//     // restrict myself within the canvas
+//     if(myX <= 25) {
+//        myX = 25;
+//     }
+    
+//     if(myX >= 475) {
+//        myX = 475;
+//     }
+
+//     if(myY <= 25) {
+//        myY = 25;
+//     }
+
+//     if(myY >= 475) {
+//        myY = 475;
+//     }
+
+//     // know my edge
+//     myLeft = myX - 25;
+//     myRight = myX + 25;
+//     myTop = myY - 25;
+//     myBottom = myY - 25; 
+
+//     // define enemy edges
+//     enemyLeft = enemyX - 25;
+//     enemyRight = enemyX + 25;
+//     enemyTop = enemyY - 25;
+//     enemyBottom = enemyY + 25; 
+
+//     // detect collision ( or rather, detect non-collision)
+//     if (myLeft > enemyRight || myRight < enemyLeft || myTop > enemyBottom || myBottom < enemyTop) {
+//         // do nothing 
+//     }
+
+//     else {
+//         // there is collision!!!
+//         console.log("Collision!");
+//         // fill random(255), random(255), random(255)); 
+//         // text("I'm colliding with my enemy. Ouch!", 255, 400) 
+
+//         //incrase pionts by 1
+//         points++;
+
+//         // generate new enemy position
+//         enemyX = random(25, 475);
+//         enemyY = random(150, 450);
+//     }
+
+//     fill(255);
+//     text("Enemies collected: ", + points, 340, 25);
+// }
