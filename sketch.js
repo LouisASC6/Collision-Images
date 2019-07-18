41dd67df9a8bbbd9ba30569da3f973f752d26a0a
LouisASC6 <louismm16@gmail.com>
2019-07-18 14:14:25 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -1,124 +1,249 @@
-// declare variables
-let myposX = 25;
-let myposY = 25;
-let myspeed = 5;
-let enemyPosX, enemyPosY;
-let enemywidth = 50, enemyheight = 50;
-let mywidth = 50;
-let myheight = 50; 
+let enemyX, enemyY;
+let enemyWidth = 50;
+let enemyHeight = 50;
+let enemyLeft, enemyRight, enemyTop, enemyBottom;
 
+let myX = 25; 
+let myY = 25;
+let myWidth = 50;
+let myHeight = 50;
+let mySpeed = 3;
 let myLeft, myRight, myTop, myBottom;
-let enemyLeft, enemyRight, enemyTop, enemyBottom; 
 
-let points = 0
+let points = 0;
 
+// graphics 
 let clefairy;
 let togepi;
+let backdrop;
 
 function setup() {
     createCanvas(500, 500);
-    background(0);
+    noStroke();
     textSize(15);
 
-    //switch rectangle
-    rectMode(CENTER);
+    // change imageMode
+    imageMode(CENTER);
 
-    // determine the random starting postiton of enemy
-    enemyPosX = random(25, 475);
-    enemyPosY = random(100, 475);
+    // randomly generate the location of enemy
+    enemyX = random(25, 475);
+    enemyY = random(150, 450);
 
+    // load images
     clefairy = loadImage("clefairy.png");
     togepi = loadImage("togepi.png");
-
+    backdrop = loadImage("forest.png");
 }
-    
+
 function draw() {
-    // refresh the background
-    background(0);
-    
-    image(togepi, enemyPosX, enemyPosY, enemywidth, enemyheight);
+    // paint the background black
+    image(backdrop, width / 2, height / 2, 500, 500);
 
-    image(clefairy, myposX, myposY, mywidth, myheight);
+    // draw the red enemy
+    fill(255, 0, 0);
+    image(togepi, enemyX, enemyY, enemyWidth, enemyHeight);
 
-    // // draw the enemy 
-    // fill(255, 0, 0);
-    // rect(enemyPosX, enemyPosY, enemywidth, enemyheight);
-    
-    // // draw myself
-    // fill(0, 0, 255);
-    // rect(myposX, myposY, 50, 50);
+    // draw yourself
+    fill(0, 0, 255);
+    image(clefairy, myX, myY, myWidth, myHeight);
 
-    // if the LEFT_ARROW is pressed 
-    if(keyIsDown(LEFT_ARROW)) {
-        myposX -= myspeed;
+    // if LEFT_ARROW is pressed
+    if (keyIsDown(LEFT_ARROW)) {
+        myX -= 3;
     }
 
-    // if the RIGHT_ARROW is pressed
-    if(keyIsDown(RIGHT_ARROW)) {
-        myposX += myspeed;
+    // if RIGHT_ARROW is pressed
+    if (keyIsDown(RIGHT_ARROW)) {
+        myX += 3;
     }
 
     // if UP_ARROW is pressed
-    if(keyIsDown(UP_ARROW)) {
-        myposY -= myspeed;
+    if (keyIsDown(UP_ARROW)) {
+        myY -= 3;
     }
 
     // if DOWN_ARROW is pressed
-    if(keyIsDown(DOWN_ARROW)) {
-        myposY += myspeed;
+    if (keyIsDown(DOWN_ARROW)) {
+        myY += 3;
     }
 
-    // restrict myself within the canvas
-    if(myposX <= 25) {
-       myposX = 25;
-    }
-    
-    if(myposX >= 475) {
-       myposX = 475;
+    // restrict your character to the canvas
+    if (myX <= 25) {
+        myX = 25;
     }
 
-    if(myposY <= 25) {
-       myposY = 25;
+    if (myX >= 475) {
+        myX = 475;
     }
 
-    if(myposY >= 475) {
-       myposY = 475;
+    if (myY <= 25) {
+        myY = 25;
     }
 
-    // know my edge
-    myLeft = myposX - 25;
-    myRight = myposX + 25;
-    myTop = myposY - 25;
-    myBottom = myposY - 25; 
+    if (myY >= 475) {
+        myY = 475;
+    }
 
-    // define enemy edges
-    enemyLeft = enemyPosX - 25;
-    enemyRight = enemyPosX + 25;
-    enemyTop = enemyPosY - 25;
-    enemyBottom = enemyPosY + 25; 
+    // define my position 
+    myLeft = myX - 25;
+    myRight = myX + 25;
+    myTop = myY - 25;
+    myBottom = myY + 25;
 
-    // detect collision ( or rather, detect non-collision)
+    // define enemy's position 
+    enemyLeft = enemyX - 25;
+    enemyRight = enemyX + 25;
+    enemyTop = enemyY - 25;
+    enemyBottom = enemyY + 25;
+
+    // detect collision!
     if (myLeft > enemyRight || myRight < enemyLeft || myTop > enemyBottom || myBottom < enemyTop) {
-        // do nothing 
+        // there's no collision here - do nothing
     }
 
     else {
-        // there is collision!!!
+        // there's collision
         console.log("Collision!");
-        // fill random(255), random(255), random(255)); 
-        // text("I'm colliding with my enemy. Ouch!", 255, 400) 
+        fill(random(255), random(255), random(255));
+        text("I'm colliding with my enemy. Ouch!", 255, 480);
 
-        //incrase pionts by 1
+        // increase points by 1
         points++;
 
         // generate new enemy position
-        enemyPosX = random(25, 475);
-        enemyPosY = random(150, 450);
+        enemyX = random(25, 475);
+        enemyY = random(150, 450);
     }
 
     fill(255);
-    text("Enemies collected: ", + points, 340, 25);
+    text("Enemies collected: " + points, 340, 475);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -133,16 +258,16 @@ function draw() {
 
 
 // // declare variables
-// let myX = 25;
-// let myY = 25;
+// let myposX = 25;
+// let myposY = 25;
 // let myspeed = 5;
-// let myHeight = 50;
-// let myWidth = 50;
-// let myLeft, myRight, myTop, myBottom;
-
-// let enemyLeft, enemyRight, enemyTop, enemyBottom; 
-// let enemyX, enemyY;
+// let enemyPosX, enemyPosY;
 // let enemywidth = 50, enemyheight = 50;
+// let mywidth = 50;
+// let myheight = 50; 
+
+// let myLeft, myRight, myTop, myBottom;
+// let enemyLeft, enemyRight, enemyTop, enemyBottom; 
 
 // let points = 0
 
@@ -156,39 +281,38 @@ function draw() {
 
 //     //switch rectangle
 //     rectMode(CENTER);
+//     imageMode(CENTER);
 
 //     // determine the random starting postiton of enemy
 //     enemyPosX = random(25, 475);
 //     enemyPosY = random(100, 475);
 
 //     clefairy = loadImage("clefairy.png");
-//     togepi = loadingimage("togepi.png");
+//     togepi = loadImage("togepi.png")
 // }
     
 // function draw() {
 //     // refresh the background
-//     background(0);
+//     image(togepi, enemyPosX, enemyPosY, enemywidth, enemyheight);
 
-//     image(togepi, enemyX, enemyY, enemywidth, enemyheight);
+//     image(clefairy, myposX, myposY, mywidth, myheight);
 
-//     image(clefairy, myX, myY, myWidth, myHeight);
+//     // // draw the enemy 
+//     // fill(255, 0, 0);
+//     // rect(enemyPosX, enemyPosY, enemywidth, enemyheight);
     
-//     // draw the enemy 
-//     fill(255, 0, 0);
-//     rect(enemyX, enemyY, enemywidth, enemyheight);
-    
-//     // draw myself
-//     fill(0, 0, 255);
-//     rect(myX, myY, 50, 50);
+//     // // draw myself
+//     // fill(0, 0, 255);
+//     // rect(myposX, myposY, 50, 50);
 
 //     // if the LEFT_ARROW is pressed 
 //     if(keyIsDown(LEFT_ARROW)) {
-//         myX -= myspeed;
+//         myposX -= myspeed;
 //     }
 
 //     // if the RIGHT_ARROW is pressed
 //     if(keyIsDown(RIGHT_ARROW)) {
-//         myX += myspeed;
+//         myposX += myspeed;
 //     }
 
 //     // if UP_ARROW is pressed
@@ -198,37 +322,37 @@ function draw() {
 
 //     // if DOWN_ARROW is pressed
 //     if(keyIsDown(DOWN_ARROW)) {
-//         myY += myspeed;
+//         myposY += myspeed;
 //     }
 
 //     // restrict myself within the canvas
-//     if(myX <= 25) {
-//        myX = 25;
+//     if(myposX <= 25) {
+//        myposX = 25;
 //     }
     
-//     if(myX >= 475) {
-//        myX = 475;
+//     if(myposX >= 475) {
+//        myposX = 475;
 //     }
 
-//     if(myY <= 25) {
-//        myY = 25;
+//     if(myposY <= 25) {
+//        myposY = 25;
 //     }
 
-//     if(myY >= 475) {
-//        myY = 475;
+//     if(myposY >= 475) {
+//        myposY = 475;
 //     }
 
 //     // know my edge
-//     myLeft = myX - 25;
-//     myRight = myX + 25;
-//     myTop = myY - 25;
-//     myBottom = myY - 25; 
+//     myLeft = myposX - 25;
+//     myRight = myposX + 25;
+//     myTop = myposY - 25;
+//     myBottom = myposY - 25; 
 
 //     // define enemy edges
-//     enemyLeft = enemyX - 25;
-//     enemyRight = enemyX + 25;
-//     enemyTop = enemyY - 25;
-//     enemyBottom = enemyY + 25; 
+//     enemyLeft = enemyPosX - 25;
+//     enemyRight = enemyPosX + 25;
+//     enemyTop = enemyPosY - 25;
+//     enemyBottom = enemyPosY + 25; 
 
 //     // detect collision ( or rather, detect non-collision)
 //     if (myLeft > enemyRight || myRight < enemyLeft || myTop > enemyBottom || myBottom < enemyTop) {
@@ -245,8 +369,8 @@ function draw() {
 //         points++;
 
 //         // generate new enemy position
-//         enemyX = random(25, 475);
-//         enemyY = random(150, 450);
+//         enemyPosX = random(25, 475);
+//         enemyPosY = random(150, 450);
 //     }
 
 //     fill(255);
